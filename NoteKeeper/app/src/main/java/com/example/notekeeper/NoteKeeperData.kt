a737a5cdd0655a9052f9b18fc123f89c77d318dd
Wami Ikechukwu <wamiikechukwu@gmail.com>
2020-05-15 14:22:27 +0100

    added a kotlin file to house our kotlin classes

--- NoteKeeper/app/src/main/java/com/example/notekeeper/NoteKeeperData.kt
+++ NoteKeeper/app/src/main/java/com/example/notekeeper/NoteKeeperData.kt
@@ -1,2 +1,9 @@
 package com.example.notekeeper
 
+class courseInfo(val courseId: String, val title: String){
+
+}
+
+class NoteInfo(val course: courseInfo, var title: String, var text: String){
+
+}
